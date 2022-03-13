ae32775d1bbc9c3849199d14520ee80d3c16e5d5
IonIon917 <pmmadjt@gmail.com>
2022-03-13 21:36:52 +0900

    migrate修正

--- db/migrate/20220306021930_add_name_profile_occupation_position_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddNameProfileOccupationPositionToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :profile, :text
-    add_column :users, :occuoation, :text
-    add_column :users, :position, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -44,28 +44,27 @@ ActiveRecord::Schema.define(version: 2022_03_09_214850) do
   end
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.text "catch_copy"
-    t.text "concept"
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.text "concept", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.text "profile"
-    t.text "occuoation"
-    t.text "position"
-    t.text "occupation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -73,5 +72,4 @@ ActiveRecord::Schema.define(version: 2022_03_09_214850) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
-  add_foreign_key "prototypes", "users"
 end
